396d1c241790ade85a913815cc2c671feb663d89
AlessioMura <alessiomura03@gmail.com>
2024-10-22 15:10:27 +0200

    added new instance and gathered info

--- script.js
+++ script.js
@@ -4,4 +4,18 @@ class Veicolo {
     colore;
     porte;
     carburante;
+
+    constructor(marca, anno, colore,) {
+        this.marca = marca;
+        this.anno = anno;
+        this.colore = colore;
+    }
+
+    informazioni() {
+        return `${this.marca} ${this.anno} ${this.colore}`
+    }
 }
+
+const new_car = new Veicolo ('Fiat', 2019, 'blu');
+console.log(new_car.informazioni());
+
